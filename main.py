f72ec0d5102bf4224968338a1d2da2f03c5b3b12
Ahsan Chhipa <163771405+ahsanchhipa@users.noreply.github.com>
2024-05-31 16:27:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+name = st.text_input("Enter Your Name: ")
+Fname=  st.text_input("Enter Your Father Name: ")
+adr= st.text_area("Enter Your Text: ")
+classdata = st.selectbox("Enter your Designation: ",(" ","SRA","RA", "PA", "RS" ) )
